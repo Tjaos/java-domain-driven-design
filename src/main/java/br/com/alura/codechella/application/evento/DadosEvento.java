d471ecae203f7c0674f92f31b25185d4aa32e8ca
Tjaos <th83287716@gmail.com>
2026-01-07 18:01:39 -0300

    refactor: refactor packages and subpackages to follow ddd method

---
+++ src/main/java/br/com/alura/codechella/application/evento/DadosEvento.java
@@ -0,0 +1,18 @@
+package br.com.alura.codechella.application.evento;
+
+import br.com.alura.codechella.domain.evento.Categoria;
+import br.com.alura.codechella.infra.ingresso.DadosCadastroTipoIngresso;
+import br.com.alura.codechella.infra.evento.DadosEndereco;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record DadosEvento(
+        Long id,
+        Categoria categoria,
+        String descricao,
+        DadosEndereco endereco,
+        LocalDateTime data,
+        List<DadosCadastroTipoIngresso> tipoIngressos
+) {
+}
