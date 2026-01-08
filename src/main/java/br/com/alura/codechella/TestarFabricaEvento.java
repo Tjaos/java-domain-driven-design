ac41201cfed9d4a1afa0d290d6ea9a7550bd5e44
Tjaos <th83287716@gmail.com>
2026-01-08 11:13:03 -0300

    feat: make evento builders to run for manual tests

---
+++ src/main/java/br/com/alura/codechella/TestarFabricaEvento.java
@@ -0,0 +1,19 @@
+package br.com.alura.codechella;
+
+import br.com.alura.codechella.domain.evento.Categoria;
+import br.com.alura.codechella.domain.evento.Evento;
+
+import java.time.LocalDateTime;
+
+public class TestarFabricaEvento {
+    public static void main(String[] args) {
+
+        Evento evento = new Evento.Builder()
+                .comCategoria(Categoria.MUSICA)
+                .comDescricao("The Weeknd")
+                .comEndereco("25989785", 50, "Maracanã")
+                .comData(LocalDateTime.parse("2025-11-19T19:00:00"))
+                .build();
+        System.out.println(evento);
+    }
+}
